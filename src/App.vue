ece8e44bba1cbb9844ea6eb4fc36014826565fe7
Abdul Mannan Khan <abdulmannan.khan005@gmail.com>
2024-04-25 22:11:53 +0500

    Navbar is added in the application

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>
+    <Navbar />
+    <v-main>
+      Hello world!
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Navbar from './components/Navbar.vue'
+
+export default {
+  name: 'App',
+
+  components: {
+    Navbar
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,25 @@
+<template>
+  <nav>
+    <v-toolbar dark app>
+      <!-- <v-toolbar-side-icon /> -->
+       <v-app-bar-nav-icon />
+      <v-toolbar-title class="text-uppercase yellow--text">
+        Mannan's <span class="font-weight-light">Mind</span> Admin
+      </v-toolbar-title>
+        <v-spacer></v-spacer>
+
+      <v-toolbar-items>
+        <v-btn outlined color="white">New Post</v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "MyNavbar",
+};
+</script>
+
+<style>
+</style>
